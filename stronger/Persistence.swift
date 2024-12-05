69abd587bb587bb559d123be1d3cebeb98767bb4
XJ <xjtan99@gmail.com>
2024-12-05 13:19:25 +0800

    Added grouping for workout list

--- stronger/Persistence.swift
+++ stronger/Persistence.swift
@@ -13,20 +13,45 @@ struct PersistenceController {
     static var preview: PersistenceController = {
         let result = PersistenceController(inMemory: true)
         let viewContext = result.container.viewContext
-        for _ in 0..<10 {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
+        
+        for i in 0..<5 {
+            let newWorkout = Workout(context: viewContext)
+            newWorkout.timestamp = Date().addingTimeInterval(TimeInterval(-i * 24 * 60 * 60))
+            newWorkout.name = "Workout \(5 - i)"
+            
+            let newWorkout2 = Workout(context: viewContext)
+            newWorkout2.timestamp = Date().addingTimeInterval(TimeInterval(-i * 24 * 60 * 60))
+            newWorkout2.name = "Workout \(5 - i) (1)"
+            
+            let exercise1 = Exercise(context: viewContext)
+            exercise1.name = "Push-Ups"
+            exercise1.reps = "15"
+            exercise1.weight = "0"
+            newWorkout.addToExercises(exercise1)
+            
+            let exercise2 = Exercise(context: viewContext)
+            exercise2.name = "Squats"
+            exercise2.reps = "20"
+            exercise2.weight = "50"
+            newWorkout.addToExercises(exercise2)
+            
+            let exercise3 = Exercise(context: viewContext)
+            exercise3.name = "Deadlifts"
+            exercise3.reps = "10"
+            exercise3.weight = "100"
+            newWorkout.addToExercises(exercise3)
         }
+        
         do {
             try viewContext.save()
         } catch {
-            // Replace this implementation with code to handle the error appropriately.
-            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
             let nsError = error as NSError
             fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
         }
+        
         return result
     }()
+
 
     let container: NSPersistentContainer
 
@@ -52,5 +77,22 @@ struct PersistenceController {
             }
         })
         container.viewContext.automaticallyMergesChangesFromParent = true
+    }
+    
+    func clearData() {
+        let entityNames = container.managedObjectModel.entities.map { $0.name! }
+        
+        for entityName in entityNames {
+            let fetchRequest: NSFetchRequest<NSFetchRequestResult> = NSFetchRequest(entityName: entityName)
+            let deleteRequest = NSBatchDeleteRequest(fetchRequest: fetchRequest)
+            
+            do {
+                try container.viewContext.execute(deleteRequest)
+                print("CoreData cleared")
+            } catch {
+                let nsError = error as NSError
+                print("Failed to delete data for entity \(entityName): \(nsError), \(nsError.userInfo)")
+            }
+        }
     }
 }
---
+++ stronger/Views/ContentView.swift
@@ -0,0 +1,141 @@
+//
+//  ContentView.swift
+//  stronger
+//
+//  Created by Tan Xin Jie on 4/12/24.
+//
+
+import SwiftUI
+import CoreData
+
+struct ContentView: View {
+    @Environment(\.managedObjectContext) private var viewContext
+
+    @FetchRequest(
+        sortDescriptors: [NSSortDescriptor(keyPath: \Workout.timestamp, ascending: false)],
+        animation: .default)
+    private var items: FetchedResults<Workout>
+
+    var body: some View {
+        TabView {
+            NavigationView {
+                List {
+                    ForEach(groupWorkoutByDay(items), id: \.0) { daysAgo, workouts in
+                        Section(header: Text("\(getDaysAgoString(daysAgo))")) {
+                            ForEach(workouts) { workout in
+                                ExtractedView(workout: workout)
+                            }
+                        }
+                    }
+                    .onDelete(perform: deleteItems)
+                }
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        EditButton()
+                    }
+                    ToolbarItem {
+                        Button(action: addItem) {
+                            Label("Add Item", systemImage: "plus")
+                        }
+                    }
+                }
+                Text("Select an item")
+            }
+            .tabItem {
+                Label("Workout", systemImage: "square.and.pencil")
+            }
+            Color.red
+                .tabItem{
+                    Label("Map", systemImage: "map")
+                }
+        }
+    }
+
+    private func addItem() {
+        withAnimation {
+//            let newItem = Workout(context: viewContext)
+//            newItem.timestamp = Date()
+
+            do {
+                try viewContext.save()
+            } catch {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                let nsError = error as NSError
+                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+            }
+        }
+    }
+
+    private func deleteItems(offsets: IndexSet) {
+        withAnimation {
+            offsets.map { items[$0] }.forEach(viewContext.delete)
+
+            do {
+                try viewContext.save()
+            } catch {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                let nsError = error as NSError
+                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+            }
+        }
+    }
+    
+    // Group workouts by the numbers of days ago
+    private func groupWorkoutByDay(_ workouts: FetchedResults<Workout>) -> [(Int, [Workout])] {
+        let grouped = Dictionary(grouping: workouts) { workout in
+            Int(workout.timestamp!.distance(to: Date()) / (24 * 60 * 60))
+        }
+        
+        // Sort the grouped workouts by days ago, convert dictionary to tuple (required to be used with ForEach List)
+        let sortedGroups = grouped.sorted { $0.key < $1.key }
+        return sortedGroups
+    }
+    
+    private func getDaysAgoString(_ input: Int) -> String {
+        return input == 0 ? "Today" : input == 1 ? "Yesterday" : "\(input) days ago"
+    }
+}
+
+private let workoutTimeFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .short
+    formatter.timeStyle = .none
+    return formatter
+}()
+
+#Preview {
+    ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+}
+
+struct ExtractedView: View {
+    let workout: Workout
+    
+    var body: some View {
+        NavigationLink {
+            VStack {
+                Text("Item at \(workout.timestamp!, formatter: workoutTimeFormatter)")
+                    .font(.title)
+                if let exercises = workout.exercises as? Set<Exercise> {
+                    ForEach(Array(exercises), id: \.self) { exercise in
+                        HStack {
+                            Text(exercise.name ?? "Unnamed Exercise")
+                            Text("\(exercise.weight ?? "?") kg")
+                            Text("\(exercise.reps ?? "?") reps")
+                        }
+                    }
+                } else {
+                    Text("No exercises found")
+                }
+            }
+            .padding()
+        } label: {
+            VStack (alignment: .leading) {
+                Text(workout.name!)
+                    .font(.headline)
+                Text(workout.timestamp!, formatter: workoutTimeFormatter)
+            }
+        }
+    }
+}
